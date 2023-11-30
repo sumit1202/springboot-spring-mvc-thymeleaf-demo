615d50598b219bc9d1396059810b04ec658a659d
sumitkumar <sumitkumar1202@gmail.com>
2023-11-30 13:09:12 +0530

    commit4

--- src/main/java/com/example/springboot/thymeleafdemo/controller/HelloWorldCotnroller.java
+++ src/main/java/com/example/springboot/thymeleafdemo/controller/HelloWorldCotnroller.java
@@ -3,6 +3,7 @@ package com.example.springboot.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -19,10 +20,17 @@ public class HelloWorldCotnroller {
         return "helloworld-process-form";
     }
 
-    @GetMapping("/displayincaps")
-    public String processFormInCaps(HttpServletRequest request, Model model) {
-        String theName = request.getParameter("studentName");
-        String result = "YO! " + theName.toUpperCase();
+    @GetMapping("/displayincaps1")
+    public String processFormInCaps1(HttpServletRequest request, Model model) {
+        String thestudentName = request.getParameter("studentName");
+        String result = "YO! " + thestudentName.toUpperCase();
+        model.addAttribute("message", result);
+        return "helloworld-process-form-in-caps";
+    }
+
+    @GetMapping("/displayincaps2")
+    public String processFormInCaps2(@RequestParam("studentName") String thestudentName, Model model) {
+        String result = "YO! " + thestudentName.toUpperCase();
         model.addAttribute("message", result);
         return "helloworld-process-form-in-caps";
     }
