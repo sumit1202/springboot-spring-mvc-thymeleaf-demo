d546360e1bde7f241bb5fcf6f11a640931f2783e
sumitkumar <sumitkumar1202@gmail.com>
2023-11-30 12:57:00 +0530

    commit3

--- src/main/java/com/example/springboot/thymeleafdemo/controller/HelloWorldCotnroller.java
+++ src/main/java/com/example/springboot/thymeleafdemo/controller/HelloWorldCotnroller.java
@@ -1,19 +1,29 @@
 package com.example.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldCotnroller {
 
-    @GetMapping("/showForm")
+    @GetMapping("/form")
     public String showForm() {
         return "helloworld-form";
     }
 
-    @GetMapping("/processForm")
+    @GetMapping("/display")
     public String processForm() {
         return "helloworld-process-form";
     }
 
+    @GetMapping("/displayincaps")
+    public String processFormInCaps(HttpServletRequest request, Model model) {
+        String theName = request.getParameter("studentName");
+        String result = "YO! " + theName.toUpperCase();
+        model.addAttribute("message", result);
+        return "helloworld-process-form-in-caps";
+    }
 }
